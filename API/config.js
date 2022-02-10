281dc2f8df13947ab41f92a2944791ec94510a2f
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-02-10 23:44:01 +0100

    add ip of server

--- API/config.js
+++ API/config.js
@@ -2,7 +2,7 @@ module.exports = {
   TypeBD: process.env.TYPEBD || "mariadb",
   BD: {
     FlieSqli: process.env.FILESQLI || "./db/Defaut.db",
-    Host: process.env.HOSTBD || "localhost",
+    Host: process.env.HOSTBD || "192.168.1.39",
     Port: process.env.PORTBD || 3306,
     User: process.env.USERBD || "root",
     Password: process.env.PASSWDBD || "root",
