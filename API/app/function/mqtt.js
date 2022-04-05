2165ee3f814c474dab9e62b0eb5ff76786c00c3f
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-04-05 11:33:16 +0200

    create funtion for plex and all

---
+++ API/app/function/mqtt.js
@@ -0,0 +1,15 @@
+const client = require("../connexionSocket");
+const mqtt = (id, value) => {
+  return new Promise((resolve, reject) => {
+    desvalue["value"] = value.value;
+    client
+      .objetExe(id, JSON.stringify(desvalue))
+      .then((value) => {
+        resolve(value);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+};
+module.exports = mqtt;
