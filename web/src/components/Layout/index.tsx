5ff1c33670238e33b8776e58fa3300572d20c0f9
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-04-12 11:28:41 +0200

    fix sidebar home pages

--- web/src/components/Layout/index.tsx
+++ web/src/components/Layout/index.tsx
@@ -1,31 +1,31 @@
-import React, { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
-import { useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../../store/store';
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../store/store";
 
-import TopBar from './TopBar';
-import SubTopBar from './SubTopBar';
-import LeftMenu from './LeftMenu';
+import TopBar from "./TopBar";
+import SubTopBar from "./SubTopBar";
+import LeftMenu from "./LeftMenu";
 
 export default function Layout() {
-  const navigate = useNavigate()
-  const { isLogged } = useAppSelector((store) => store.auth)
-  
+  const navigate = useNavigate();
+  const { isLogged } = useAppSelector((store) => store.auth);
+
   useEffect(() => {
-    if(!isLogged){
-      navigate('/login')
+    if (!isLogged) {
+      navigate("/login");
     }
-  }, [isLogged, navigate])
+  }, [isLogged, navigate]);
 
   return (
     <div className="max-h-full h-full flex flex-row max-w-full">
-      <LeftMenu/>
-      <div className='flex flex-col w-full ml-0.5'>
-        <TopBar/>
-        <SubTopBar/>
-        <div className='h-full overflow-y-scroll'>
-          <Outlet/>
+      <LeftMenu />
+      <div className="flex flex-col w-full ml-0.5">
+        <TopBar />
+        <SubTopBar />
+        <div className="h-full overflow-y-scroll">
+          <Outlet />
         </div>
       </div>
     </div>
