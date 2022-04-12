56ac6f7e3fa85aafa102da1f99cdb17e5d67889e
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-04-12 17:09:23 +0200

    formatages

--- web/src/data/objectApi.ts
+++ web/src/data/objectApi.ts
@@ -1,37 +1,40 @@
 import API from "./API";
 
 export async function getObjects() {
-    try {
-        const response = await API.get('/client')
-        return Promise.resolve(response.data)
-    } catch (err) {
-        return Promise.reject(err)
-    }
+  try {
+    const response = await API.get("/client");
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export async function getCache() {
-    try {
-        const response = await API.get('/cache')
-        return Promise.resolve(response.data)
-    } catch (err) {
-        return Promise.reject(err)
-    }
+  try {
+    const response = await API.get("/cache");
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export async function getExeId(idClient: number) {
-    try {
-        const response = await API.get(`/exe/${idClient}`)
-        return Promise.resolve(response.data[0].id)
-    } catch (err) {
-        return Promise.reject(err)
-    }
+  try {
+    const response = await API.get(`/exe/${idClient}`);
+    return Promise.resolve(response.data[0].id);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export async function setObject(id: number, param: any) {
-    try {
-        const response = await API.post(`/objetexe`, {param: JSON.stringify(param), id: id})
-        return Promise.resolve(response.data)
-    } catch (err) {
-        return Promise.reject(err)
-    }
+  try {
+    const response = await API.post(`/objetexe`, {
+      param: JSON.stringify(param),
+      id: id,
+    });
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
