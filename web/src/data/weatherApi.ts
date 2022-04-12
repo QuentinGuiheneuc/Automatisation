783764d3936374ce86ad3192a7c68d1d358dc52f
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-04-12 17:08:05 +0200

    add donner, param

--- web/src/data/weatherApi.ts
+++ web/src/data/weatherApi.ts
@@ -1,14 +1,42 @@
 import API from "./API";
 
 export const weatherAPI = {
-    searchCity
-}
+  searchCity,
+  donnerMeteo,
+  saveCity,
+  selectCity,
+};
 
 export async function searchCity(city: string) {
-    try {
-        const response = await API.get(`/meteo/search?search=${city}`)
-        return Promise.resolve(response.data)
-    } catch (err) {
-        return Promise.reject(err)
-    }
+  try {
+    const response = await API.get(`/meteo/search?search=${city}`);
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+export async function donnerMeteo() {
+  try {
+    const response = await API.get(`/meteo/donner`);
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export async function saveCity(allCity: object) {
+  try {
+    const response = await API.post(`/meteo/param`, allCity);
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+export async function selectCity() {
+  try {
+    const response = await API.get(`/meteo/param`);
+    return Promise.resolve(response.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
