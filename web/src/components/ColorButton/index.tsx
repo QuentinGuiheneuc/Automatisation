f471a6eb5f3b4246a2a4257919627009912c1d57
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-04-12 17:06:36 +0200

    add 2 colors

--- web/src/components/ColorButton/index.tsx
+++ web/src/components/ColorButton/index.tsx
@@ -1,48 +1,60 @@
 import React, { useState } from "react";
 
-import { ChromePicker } from 'react-color';
+import { ChromePicker } from "react-color";
 
-import { ConnectedObject } from '../../types/ConnectedObject';
+import { ConnectedObject } from "../../types/ConnectedObject";
 
-import { getExeId, getObjects, setObject } from '../../data/objectApi';
+import { getExeId, getObjects, setObject } from "../../data/objectApi";
 
-import { useAppDispatch } from '../../store/store'
-import { setLights } from '../../states/objectSlice'
+import { useAppDispatch } from "../../store/store";
+import { setLights } from "../../states/objectSlice";
 
 type props = {
-    lightId: number
-    color: string
-    handleColor: (color: string) => void
-}
+  commende: string;
+  lightId: number;
+  color: string;
+  handleColor: (color: string) => void;
+};
 
-export default function ColorButton({lightId, color, handleColor}: props) {
-    const dispatch = useAppDispatch()
-    const [isColorPicker, setIsColorPicker] = useState(false)
-    const handleClick = (): void => {
-        setIsColorPicker(!isColorPicker)
-    }
+export default function ColorButton({
+  commende,
+  lightId,
+  color,
+  handleColor,
+}: props) {
+  const dispatch = useAppDispatch();
+  const [isColorPicker, setIsColorPicker] = useState(false);
+  const handleClick = (): void => {
+    setIsColorPicker(!isColorPicker);
+  };
 
-    const handleChangeComplete = (newColor: string) => {
-        getExeId(lightId)
-          .then((responses) => {
-                setObject(responses, {"value": `&A=${newColor}`})
-          })
-    }
+  const handleChangeComplete = (newColor: string) => {
+    getExeId(lightId).then((responses) => {
+      setObject(responses, { value: `${commende}=${newColor}` });
+    });
+  };
 
-    return(
-        <div className="relative">
-            <div className={`h-4 w-4 rounded-full cursor-pointer`} style={{background: `${color}`}}onClick={handleClick}/>
-            {isColorPicker &&
-                <div className="absolute z-10 top-5 left-2">
-                    <div className="fixed right-0 top-0 bottom-0 left-0" onClick={handleClick}/>
-                    <ChromePicker
-                        color={color}
-                        disableAlpha={true}
-                        onChange={(newColor) => handleColor(newColor.hex)}
-                        onChangeComplete={(newColor) => handleChangeComplete(newColor.hex)}
-                    />
-                </div>
-            }
+  return (
+    <div className="relative">
+      <div
+        className={`h-4 w-4 rounded-full cursor-pointer`}
+        style={{ background: `${color}` }}
+        onClick={handleClick}
+      />
+      {isColorPicker && (
+        <div className="absolute z-10 top-5 left-2">
+          <div
+            className="fixed right-0 top-0 bottom-0 left-0"
+            onClick={handleClick}
+          />
+          <ChromePicker
+            color={color}
+            disableAlpha={true}
+            onChange={(newColor) => handleColor(newColor.hex)}
+            onChangeComplete={(newColor) => handleChangeComplete(newColor.hex)}
+          />
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- web/src/components/LightCard/index.tsx
+++ web/src/components/LightCard/index.tsx
@@ -1,99 +1,135 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { ConnectedObject } from '../../types/ConnectedObject';
-import { Cache } from '../../types/Cache';
+import { ConnectedObject } from "../../types/ConnectedObject";
+import { Cache } from "../../types/Cache";
 
-import { getExeId, getObjects, setObject } from '../../data/objectApi';
+import { getExeId, getObjects, setObject } from "../../data/objectApi";
 
-import light_close_icon from '../../assets/icons/light_close_icon.svg'
-import light_open_icon from '../../assets/icons/light_open_icon.svg'
-import disconnected_icon from '../../assets/icons/disconnected_icon.svg'
+import light_close_icon from "../../assets/icons/light_close_icon.svg";
+import light_open_icon from "../../assets/icons/light_open_icon.svg";
+import disconnected_icon from "../../assets/icons/disconnected_icon.svg";
 
-import { useAppDispatch } from '../../store/store'
-import { setLights } from '../../states/objectSlice'
+import { useAppDispatch } from "../../store/store";
+import { setLights } from "../../states/objectSlice";
 
-import Slider from '@mui/material/Slider'
-import ColorButton from '../ColorButton'
+import Slider from "@mui/material/Slider";
+import ColorButton from "../ColorButton";
 
 type props = {
-  light: ConnectedObject
-  cache: Cache
-}
+  light: ConnectedObject;
+  cache: Cache;
+};
 
 export default function LightCard({ light, cache }: props) {
   const valueToHex = (value: string) => {
     var hex = parseInt(value).toString(16);
     return hex.length == 1 ? "0" + hex : hex;
-  }
+  };
 
   const rgbToHex = (r: string, g: string, b: string): string => {
     return "#" + valueToHex(r) + valueToHex(g) + valueToHex(b);
-  }
+  };
 
-  const [isOpen, setIsOpen] = useState(cache.value.v?.vs?.ac > 0)
-  const [color, setColor] = useState(rgbToHex(cache.value.v?.vs?.cl[0], cache.value.v?.vs?.cl[1], cache.value.v?.vs?.cl[2]))
-  const [brightness, setBrightness] = useState(cache.value.v?.vs?.ac / 2.55)
+  const [isOpen, setIsOpen] = useState(cache.value.v?.vs?.ac > 0);
+  const [color, setColor] = useState(
+    rgbToHex(
+      cache.value.v?.vs?.cl[0],
+      cache.value.v?.vs?.cl[1],
+      cache.value.v?.vs?.cl[2]
+    )
+  );
+  const [color2, setColor2] = useState(
+    rgbToHex(
+      cache.value.v?.vs?.cs[0],
+      cache.value.v?.vs?.cs[1],
+      cache.value.v?.vs?.cs[2]
+    )
+  );
+  const [brightness, setBrightness] = useState(cache.value.v?.vs?.ac / 2.55);
 
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    setBrightness(brightness / 2.55)
-  }, [])
+    setBrightness(brightness / 2.55);
+  }, []);
 
   const handleColor = (color: string): void => {
-    setColor(color)
-  }
-
+    setColor(color);
+  };
+  const handleColor2 = (color: string): void => {
+    setColor2(color);
+  };
   const handleBrightness = (brightness: number): void => {
-    setBrightness(brightness)
-  }
+    setBrightness(brightness);
+  };
 
   const handleBrightnessCommitted = (brightness: number) => {
-    const newBrightness: number = brightness * 2.
-    getExeId(light.id_client)
-          .then((responses) => {
-                setObject(responses, {"value": `&SB=${newBrightness}`})
-          })
-  }
+    const newBrightness: number = brightness * 2;
+    getExeId(light.id_client).then((responses) => {
+      setObject(responses, { value: `&A=${newBrightness}` });
+    });
+  };
 
   const handleLight = () => {
-    getExeId(light.id_client)
-      .then(async(responses) => {
-        await setObject(responses, {"value": isOpen? "&T=0":"&T=1"})
-                .then((responses) => {
-                  setIsOpen(!isOpen)
-                })
-        await getObjects()
-                .then((responses: Array<ConnectedObject>) => {
-                  dispatch(setLights(responses))
-                })
-      })
-  }
+    getExeId(light.id_client).then(async (responses) => {
+      await setObject(responses, { value: isOpen ? "&T=0" : "&T=1" }).then(
+        (responses) => {
+          setIsOpen(!isOpen);
+        }
+      );
+      await getObjects().then((responses: Array<ConnectedObject>) => {
+        dispatch(setLights(responses));
+      });
+    });
+  };
   return (
     <>
-      {cache &&
-        <div className='relative bg-grey-dark max-w-max p-5 ml-2 mt-2'>
-          {cache.value.status === 'offline' &&
+      {cache && (
+        <div className="relative bg-grey-dark max-w-max p-5 ml-2 mt-2">
+          {cache.value.status === "offline" && (
             <>
-              <img src={disconnected_icon} className='absolute right-2 top-2 h-5 w-5 z-50'/>
-              <div className='absolute w-full h-full bg-black-transparent top-0 left-0'/>
+              <img
+                src={disconnected_icon}
+                className="absolute right-2 top-2 h-5 w-5 z-50"
+              />
+              <div className="absolute w-full h-full bg-black-transparent top-0 left-0" />
             </>
-          }
-          <img src={isOpen? light_open_icon : light_close_icon} onClick={handleLight} alt='light icon' className='w-28 h-28 mb-2 cursor-pointer' />
-          <span className='text-grey-light text-xl'>{light.client}</span>
-          <div className='flex items-center'>
-            <div className='flex w-5/6 items-center mr-1'>
-              <Slider 
+          )}
+          <img
+            src={isOpen ? light_open_icon : light_close_icon}
+            onClick={handleLight}
+            alt="light icon"
+            className="w-28 h-28 mb-2 cursor-pointer"
+          />
+          <span className="text-grey-light text-xl">{light.client}</span>
+          <div className="flex items-center">
+            <div className="flex w-5/6 items-center mr-1">
+              <Slider
                 size="small"
                 value={brightness}
-                onChangeCommitted={(event, brightness) => handleBrightnessCommitted(brightness as number)} 
-                onChange={(event, brightness) => handleBrightness(brightness as number)}/>
+                onChangeCommitted={(event, brightness) =>
+                  handleBrightnessCommitted(brightness as number)
+                }
+                onChange={(event, brightness) =>
+                  handleBrightness(brightness as number)
+                }
+              />
             </div>
-            <ColorButton lightId={light.id_client} color={color} handleColor={handleColor} />
+            <ColorButton
+              commende="&CL"
+              lightId={light.id_client}
+              color={color}
+              handleColor={handleColor}
+            />
+            <ColorButton
+              commende="&C2"
+              lightId={light.id_client}
+              color={color2}
+              handleColor={handleColor2}
+            />
           </div>
         </div>
-      }
+      )}
     </>
-
   );
 }
