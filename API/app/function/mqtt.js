d9fa74b999f9eed8ef87353fd24ca9614d00f21a
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-07-22 10:02:38 +0200

    fix mqtt plex

--- API/app/function/mqtt.js
+++ API/app/function/mqtt.js
@@ -1,6 +1,7 @@
 const client = require("../connexionSocket");
 const mqtt = (id, value) => {
   return new Promise((resolve, reject) => {
+    let desvalue;
     desvalue["value"] = value.value;
     client
       .objetExe(id, JSON.stringify(desvalue))
